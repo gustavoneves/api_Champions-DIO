a0174e048debfdbcfa653525716483e4528e9af5
gustavoneves <gustavo.neves@yandex.com>
2025-07-30 10:32:17 -0300

    Refatoracao; Implementacao de uma funcao para obtencao do jogador pelo id

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
-import { getPlayerService } from "../services/players-service";
-import { ok } from "../utils/http-helper";
+import { getPlayerByIdService, getPlayerService } from "../services/players-service";
 
 export const getPlayer = async (request: Request, response: Response) => {
     const httpResponse = await getPlayerService();
+    response.status(httpResponse.statusCode).json(httpResponse.body);
+}
+
+export const getPlayerById = async (request: Request, response: Response) => {
+    const id = parseInt(request.params.id);
+    const httpResponse = await getPlayerByIdService(id);
     response.status(httpResponse.statusCode).json(httpResponse.body);
 }

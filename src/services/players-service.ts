0284fef2ac292ab05dee1eb7e050a9d03f585ac7
gustavoneves <gustavo.neves@yandex.com>
2025-07-30 10:55:55 -0300

    Implementacao dos services para obtencao de todos os jogadores e para obtencao de um jogador pelo id

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,7 +1,23 @@
+import { findAllPlayers, findPlayerById } from "../repositories/player-repository";
 import { noContent, ok } from "../utils/http-helper";
 
 export const getPlayerService = async () => {
-    const data = { player: "ronaldo" };
+    // const data = { player: "ronaldo" };
+    const data = await findAllPlayers();
+    let res;
+    if(data){
+        res = await ok(data);
+    }
+    else{
+        res = await noContent();
+    }
+    
+    return res;
+}
+
+
+export const getPlayerByIdService = async (id: number) => {
+    const data = await findPlayerById(id);
     let res;
     if(data){
         res = await ok(data);

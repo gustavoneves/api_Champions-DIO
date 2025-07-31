e7c9cd4f42ba771c060076306fd5c339f29ed00e
gustavoneves <gustavo.neves@yandex.com>
2025-07-31 11:38:31 -0300

    Criacao do service para a remocao de um jogador pelo id

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,5 +1,5 @@
 import { PlayerModel } from "../models/player-model";
-import { findAllPlayers, findPlayerById, insertPlayer } from "../repositories/player-repository";
+import { deleteOnePlayer, findAllPlayers, findPlayerById, insertPlayer } from "../repositories/player-repository";
 import { badRequest, created, noContent, ok } from "../utils/http-helper";
 
 export const getPlayerService = async () => {
@@ -44,3 +44,23 @@ export const createPlayerService = async (player: PlayerModel) => {
     return response;
 }
 
+export const deletePlayerService = async (id: number) => {
+    let response = null;
+    const status = await deleteOnePlayer(id);
+
+    if(status !== -1){
+        response = await ok({message: "deleted"});
+    }
+    else{
+        response = await badRequest();
+    }
+
+    // if(data){
+    //     response = await ok(data);
+    // }
+    // else{
+    //     response = await badRequest();
+    // }
+    
+    return response;
+}

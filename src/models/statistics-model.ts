f822419feb933e7aa052e89999cfc512e8699b4c
gustavoneves <gustavo.neves@yandex.com>
2025-07-31 12:18:51 -0300

    Criacao do model para as estatisticas do jogador

---
+++ src/models/statistics-model.ts
@@ -0,0 +1,11 @@
+export interface statisticsModel {
+    statistics: {
+        Overall: number;
+        Pace: number;
+        Shooting: number;
+        Passing: number;
+        Dribbling: number;
+        Defending: number;
+        Physical: number;
+    };
+};

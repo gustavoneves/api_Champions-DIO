5940f5e7705486e5195ba6109f6c9ab980ca6536
gustavoneves <gustavo.neves@yandex.com>
2025-08-04 14:21:00 -0300

    Criacao do controller para obter todos os clubes

---
+++ src/controllers/clubs-controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express"
+import { getClubService } from "../services/clubs-service"
+
+export const getClubs = async (request: Request, response: Response) => {
+    const res = await getClubService();
+
+    response.status(res.statusCode).json(res.body);
+}

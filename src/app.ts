a27e73ad489795f26a61636bb8df1835ae93c93a
gustavoneves <gustavo.neves@yandex.com>
2025-08-04 14:43:05 -0300

    Adicao cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { json } from "express";
+import cors from "cors";
+
 import router from "./routes";
+
 
 function createApp(){
     const app = express();
 
+    app.use(cors());
     app.use(json());
 
     app.use("/api", router);
